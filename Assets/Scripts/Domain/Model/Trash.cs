e5e41f67663d145136313487b8e9e4b61fb831ee
torobi <torobi2222@gmail.com>
2023-01-15 00:35:13 +0900

    ADD: Trashのドメイン

--- Assets/Scripts/Domain/Model/Trash.cs
+++ Assets/Scripts/Domain/Model/Trash.cs
@@ -2,6 +2,30 @@ namespace Domain.Model
 {
     public class Trash
     {
-        
+        private const int CoolTimeTurn = 3;
+        private int _remainTime = 0; // 0でTrash可能
+        private bool _canTrash = true;
+
+        public void ReduceRemain()
+        {
+            if (_canTrash) return;
+
+            _remainTime -= 1;
+            if (_remainTime <= 0)
+            {
+                _canTrash = true;
+            }
+        }
+
+        public void TrashMino()
+        {
+            _canTrash = false;
+            _remainTime = CoolTimeTurn;
+        }
+
+        public float Remain()
+        {
+            return (float)_remainTime / CoolTimeTurn;
+        }
     }
 }
